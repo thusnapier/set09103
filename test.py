0f3dcdbdffebb07f067b4b273eb844b9c5f9916b
thusnapier <40207956@live.napier.ac.uk>
2017-09-28 10:35:46 +0000

    Hello app

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-from flask import Flask, redirect, url_for, abort
+from flask import Flask, abort
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-  return "The default, 'root' route"
-
-@app.route("/hello/")
 def hello():
   return "Hello Napier!"
 
-@app.route("goodbye")
-def goodbye():
-  return "Bye"
+@app.route("/force404/")
+def force404():
+  abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return "Page not found :(", 404
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
