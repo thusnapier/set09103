7130b912515f03c8618f5069230a9c87cfb0c057
thusnapier <40207956@live.napier.ac.uk>
2017-09-28 10:25:50 +0000

    Hello app

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, abort
 app = Flask(__name__)
 
 @app.route("/")
+def root():
+  return "The default, 'root' route"
+
+@app.route("/hello/")
 def hello():
-  return "Hello World!"
+  return "Hello Napier!"
+
+@app.route("goodbye")
+def goodbye():
+  return "Bye"
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', debug=True)
