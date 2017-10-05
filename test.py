ca5bd1d18b44d72dd5e6bef5c0dc0d87ac82913b
thusnapier <40207956@live.napier.ac.uk>
2017-10-05 09:59:21 +0000

    test app

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
-from flask import Flask, abort
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello Napier!"
+def root():
+  return "The default, 'root' route"
 
-@app.route("/force404/")
-def force404():
-  abort(404)
-
-@app.errorhandler(404)
-def page_not_found(error):
-  return "Page not found :(", 404
+@app.route("/account/", methods=['GET', 'POST'])
+def account():
+  if request.method == 'POST':
+    return "POST'ed to /account root\n"
+  else:
+    return "GET /account root"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
